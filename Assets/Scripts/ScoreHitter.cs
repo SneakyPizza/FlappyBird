67b07e549c2a05f0ff0425010f50341e823a2d56
Ramses di Perna <15826@ma-web.nl>
2016-01-14 09:27:54 +0100

    Made Score base and adding after passing tube

---
+++ Assets/Scripts/ScoreHitter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreHitter : MonoBehaviour {
+
+	private Score _score;
+	private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindGameObjectWithTag("Player");
+		_score = GameObject.FindGameObjectWithTag ("Score").GetComponent<Score> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_player.gameObject.transform.position.x >= this.gameObject.transform.position.x) {
+			_score.AddScore();
+			Destroy(this);
+		}
+	}
+}
