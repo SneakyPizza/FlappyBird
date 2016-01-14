9bd43b4e500dac6baa47c90170427114e205f216
Lerondo <alessandrojbruin@hotmail.com>
2016-01-14 09:19:13 +0100

    fixes

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+
+	public List<AudioClip> audioList = new List<AudioClip>();
+}
