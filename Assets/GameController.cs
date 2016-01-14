f824e8790619c72b7686a483240c469a0404ab50
menno <run_jongejan@hotmail.com>
2016-01-14 09:28:16 +0100

    gamecontroller

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
-
+    public GameObject tubeContainer;
 	// Use this for initialization
 	void Start () {
         GameObject player = GameObject.FindGameObjectWithTag(Tags.PLAYER);
@@ -10,9 +10,21 @@ public class GameController : MonoBehaviour {
         player.GetComponent<PlayerBounds>().DeathEvent += LoseGame;
     }
 	
+    private void StartGame()
+    {
+        //TODO: on first press start game
+    }
+
 	private void LoseGame()
     {
         //TODO: reset score
-        //TODO: reset level
+        Transform[] childTransforms = tubeContainer.GetComponentsInChildren<Transform>();
+        foreach(Transform childTransform in childTransforms)
+        {
+            if(childTransform != this.transform)
+            {
+                Destroy(childTransform.gameObject);
+            }
+        }
     }
 }
