0594fa33bc44d719d77701368034fa29c0ff7f87
menno <run_jongejan@hotmail.com>
2016-01-14 09:25:49 +0100

    bounds + collision + loseevent in gamecontroller

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+    public delegate void NormalDelegate();
+    public event NormalDelegate DeathEvent;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.tag == Tags.TUBE)
+        {
+            if (DeathEvent != null)
+                DeathEvent();
+        }
+    }
+}
