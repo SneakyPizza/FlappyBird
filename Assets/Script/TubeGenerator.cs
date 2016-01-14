18e50aca77fafbfe56e386cce05ede757bc03ca6
menno <run_jongejan@hotmail.com>
2016-01-14 09:14:56 +0100

    TubeGeneration

---
+++ Assets/Script/TubeGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TubeGenerator : MonoBehaviour {
+    public GameObject tube;
+    public float spawnSpeed;
+    public float horizontalSpawnDistance;
+    public GameObject tubeContainer;
+
+    private float oldX = 0;
+    private float maxY = 5;
+    private float minY = -5;
+    private float currentSpawnTimer = 0;
+    void Update()
+    {
+        if(currentSpawnTimer < Time.time)
+        {
+            currentSpawnTimer = spawnSpeed + Time.time;
+            Vector3 spawnPos = new Vector3(horizontalSpawnDistance + oldX, Random.Range(minY, maxY), 0);
+            GameObject newTube = Instantiate(tube, spawnPos, Quaternion.identity) as GameObject;
+            newTube.transform.parent = tubeContainer.transform;
+        }
+    }
+}
